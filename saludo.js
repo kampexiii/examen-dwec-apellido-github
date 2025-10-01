b057f0ab597beb6323c88b1ed3872dbf5993f2b4
kampexiii <pablosevillanoa90@gmail.com>
2025-10-01 09:10:54 +0200

    añadir mensaje de motivación

--- saludo.js
+++ saludo.js
@@ -6,3 +6,8 @@ let apellido2;
 
 //Muestra por consola Hola Nombre Apellido1 Apellido2!:
     console.log("Hola " + nombre + apellido1 + apellido2);
+
+
+//Modifica el archivo para que también muestre por consola:
+    console.log("Voy a aprobar el módulo de DWEC ");
+
