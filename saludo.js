dcfa96940f8d7ba612b2b05d461b06ba40103e47
kampexiii <pablosevillanoa90@gmail.com>
2025-10-01 09:48:19 +0200

    inicializacion de variables tras consulta con el profesor

--- saludo.js
+++ saludo.js
@@ -1,8 +1,8 @@
 //Declara tres variables: nombre, apellido1, apellido2.
 
-let nombre;
-let apellido1;
-let apellido2;
+let nombre = "Pablo";
+let apellido1 =  "Sevillano";
+let apellido2 = "Aparicio";
 
 //Muestra por consola Hola Nombre Apellido1 Apellido2!:
     console.log("Hola " + nombre + apellido1 + apellido2);
