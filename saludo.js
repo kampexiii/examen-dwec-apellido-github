4860912abc4c7e0af4a75f4751c95ab7e3d5846c
kampexiii <pablosevillanoa90@gmail.com>
2025-10-01 09:09:52 +0200

    saludo inicial con nombre y apellidos

---
+++ saludo.js
@@ -0,0 +1,8 @@
+//Declara tres variables: nombre, apellido1, apellido2.
+
+let nombre;
+let apellido1;
+let apellido2;
+
+//Muestra por consola Hola Nombre Apellido1 Apellido2!:
+    console.log("Hola " + nombre + apellido1 + apellido2);
