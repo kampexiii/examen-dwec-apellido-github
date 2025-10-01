5f49d4fd400a06f58b0141c25cfe190ce2802af2
kampexiii <pablosevillanoa90@gmail.com>
2025-10-01 09:51:43 +0200

    inicializacion de variables tras consulta con el profesor

--- saludo.js
+++ saludo.js
@@ -5,9 +5,9 @@ let apellido1 =  "Sevillano";
 let apellido2 = "Aparicio";
 
 //Muestra por consola Hola Nombre Apellido1 Apellido2!:
-    console.log("Hola " + nombre + apellido1 + apellido2);
+console.log("Hola " + nombre + apellido1 + apellido2 + "!");
 
 
 //Modifica el archivo para que también muestre por consola:
-    console.log("Voy a aprobar el módulo de DWEC ");
+console.log("Voy a aprobar el módulo de DWEC ");
 
