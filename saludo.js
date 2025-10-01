e451215081b42c6e3e04e4cd224499e27bf4d154
kampexiii <pablosevillanoa90@gmail.com>
2025-10-01 09:48:48 +0200

    inicializacion de variables tras consulta con el profesor

--- saludo.js
+++ saludo.js
@@ -1,13 +1,13 @@
 //Declara tres variables: nombre, apellido1, apellido2.
 
-let nombre;
-let apellido1;
-let apellido2;
+let nombre = "Pablo";
+let apellido1 =  "Sevillano";
+let apellido2 = "Aparicio";
 
 //Muestra por consola Hola Nombre Apellido1 Apellido2!:
-    console.log("Hola " + nombre + apellido1 + apellido2);
+console.log("Hola " + nombre + apellido1 + apellido2);
 
 
 //Modifica el archivo para que también muestre por consola:
-    console.log("Voy a aprobar el módulo de DWEC ");
+console.log("Voy a aprobar el módulo de DWEC ");
 
